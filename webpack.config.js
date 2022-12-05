00813c9cefbf072df25fd48875eafb8dfc958d88
lsmnt <leasimonet56@gmail.com>
2022-12-05 15:28:05 +0100

    add assets loading and change package.json

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
                   }
                 ]
               },
+              {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+              },
         ]
     },
     plugins: [
